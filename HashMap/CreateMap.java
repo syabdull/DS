5bbc06053ac7a8c48cb42fab75817d5fa599c179
syabdull <syab5050@gmail.com>
2022-09-20 19:43:48 +0530

    first map

---
+++ HashMap/CreateMap.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+
+public class CreateMap {
+
+     public static  void   main(String[]args){
+        HashMap<String,String>capCities=new HashMap<>();
+        capCities.put("India","Delhi");
+        capCities.put("TS","Hyd");
+        capCities.put("SriLanks","colombo");
+
+    System.out.println(capCities);
+         
+     }
+    
+}
